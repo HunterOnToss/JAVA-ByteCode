6955f5ec687ec1c7cd69ddcb40b8bfff01513d5e
Stanislav.Kablukov <stasdolg@mail.ru>
2016-06-17 01:51:00 +0600

    Первая практическая работа. Генерация байт кода для простого класса
    У класса есть один единственный метод который принимает два числа и
    складывает их

---
+++ src/hunter/rae/ByteCode/ClassGen.java
@@ -0,0 +1,13 @@
+package hunter.rae.ByteCode;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import java.io.FileOutputStream;
+
+/**
+ * Created by hunter on 17.06.16.
+ */
+public class ClassGen {
+
+}
