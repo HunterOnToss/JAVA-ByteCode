fa39e187f0cd063bf635f5601a585e63457baf41
Stanislav.Kablukov <stasdolg@mail.ru>
2016-06-18 01:59:29 +0600

    Разобрал цикл for

--- src/hunter/rae/ByteCode/Foo.java
+++ src/hunter/rae/ByteCode/Foo.java
@@ -7,4 +7,49 @@ public class Foo {
 
     private String m = "Hello world";
 
+    public static int forLoop() {
+        int result = 0;
+        for (int i = 0; i < 10; i++) {
+            result = result + i;
+        }
+
+        return result;
+    }
+
+//    public static forLoop()I
+//    L0
+//    LINENUMBER 11 L0
+//            ICONST_0
+//    ISTORE 0
+//    L1
+//    LINENUMBER 12 L1
+//            ICONST_0
+//    ISTORE 1
+//    L2
+//    FRAME APPEND [I I]
+//    ILOAD 1
+//    BIPUSH 10 -- Кладет 10 на стек что бы занимать 2 байта, а не 4.
+//    IF_ICMPGE L3
+//    L4
+//    LINENUMBER 13 L4
+//    ILOAD 0
+//    ILOAD 1
+//    IADD
+//    ISTORE 0
+//    L5
+//    LINENUMBER 12 L5
+//    IINC 1 1
+//    GOTO L2
+//    L3
+//    LINENUMBER 16 L3
+//    FRAME CHOP 1
+//    ILOAD 0
+//    IRETURN
+//            L6
+//    LOCALVARIABLE i I L2 L3 1
+//    LOCALVARIABLE result I L1 L6 0
+//    MAXSTACK = 2
+//    MAXLOCALS = 2
+
+
 }
