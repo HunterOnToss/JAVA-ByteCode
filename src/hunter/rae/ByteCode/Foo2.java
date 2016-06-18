92e955415e6047a1a0f644f915340380db210856
Stanislav.Kablukov <stasdolg@mail.ru>
2016-06-18 11:59:20 +0600

    байт код switch case. Разбор

--- src/hunter/rae/ByteCode/Foo2.java
+++ src/hunter/rae/ByteCode/Foo2.java
@@ -7,4 +7,39 @@ public class Foo2 {
 
     private String m = new String("Hi!");
 
+
+    public static int negative(int i) {
+
+        switch (i) {
+            case 1: return -1;
+            case 2: return -2;
+            case 3: return -3;
+            case 4: return -4;
+            default: return 0;
+        }
+
+    }
+
+    public static int negative2(int i) {
+
+        switch (i) {
+            case 1: return -1;
+            case 2: return -2;
+            case 3: return -3;
+            case 4: return -4;
+            case 100: return -100;
+            default: return 0;
+        }
+    }
+
+    public static String nagetiveString (String i) {
+
+        switch (i) {
+            case "a": return "-a";
+            case "b": return "-b";
+
+            default: return "";
+        }
+
+    }
 }
